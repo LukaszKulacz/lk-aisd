1d6b461327351e35eccb00c440a7b1d8e808910d
LukaszKulacz <lukaszkulacz@gmail.com>
2020-01-28 21:12:38 +0100

    First working version

--- lk-aisd/urls.py
+++ lk-aisd/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-
 from django.contrib import admin
 
 admin.autodiscover()
 
 import main.views
+
 
 # To add a new path, first import the app:
 # import blog
@@ -18,3 +18,5 @@ urlpatterns = [
     path("", main.views.index, name="index"),
     path("admin/", admin.site.urls),
 ]
+
+
--- main/views.py
+++ main/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Lesson, Link
+from .models import Lesson, Link, Settings
 # Create your views here.
+
 def index(request):
 	context = {'lessons' : Lesson.objects.filter(show=True).order_by('week'), 
-			   'links' : Link.objects.all().order_by('order')}
+			   'links' : Link.objects.all().order_by('order'),
+               'settings': Settings.objects.all()}
+	def week_to_str(week):
+		if week==0:
+			return 'Poniedziałek'
+		elif week==1:
+			return 'Wtorek'
+		elif week==2:
+			return 'Środa'
+		elif week==3:
+			return 'Czwartek'
+		elif week==4:
+			return 'Piątek'
+		elif week==5:
+			return 'Sobota'
+		elif week==6:
+			return 'Niedziela'	
+ 
+	for i in context['settings']:
+		i.date = week_to_str(i.date.weekday())
 	for i in context['lessons']:
 		if len(i.tags) > 0:
 			i.tags = i.tags.split(";;")
